6db710225e65394ce26cc1bf1c9b7737a88f45e6
Nilufer Polat <niluferplt@gmail.com>
2015-09-06 13:24:32 -0400

    prework - 5/8 Q8

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -7,11 +7,27 @@
 
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+fb = csv.reader(open("football.csv", "rb"))
+goals = []
+goals_allowed=[]
+team=[]
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+for data in fb:
+    goals.append(data[5])
+    goals_allowed.append(data[6])
+    team.append(data[0])
+    
+del goals[0]
+del goals_allowed[0]
+del team[0]    
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+goals=map(int,goals)
+goals_allowed=map(int,goals_allowed)
+
+dif= [a - b for a, b in zip(goals, goals_allowed)]
+
+comb=zip(dif,team)
+comb.sort()
+
+print comb[0]
+
