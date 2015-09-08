dd9c0400c5ec8b1dfed3144eebe82c018c59d3b1
Nilufer Polat <niluferplt@gmail.com>
2015-09-07 23:53:22 -0400

    prework - 6/8 - Part III Q7

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -49,3 +49,60 @@ for key, value in x:
     print key, value
     
     
+#Q7
+
+import csv
+import collections
+from itertools import islice
+
+faculty = csv.reader(open("faculty.csv", "rb"))
+
+degree=[]
+titles=[]
+lastn=[]
+email=[]
+firstn=[]
+
+for data in faculty:
+    degree.append(data[1])
+    titles.append(data[2])
+    lastn.append(data[0])    
+    email.append(data[3])
+    firstn.append(data[0])
+    
+del degree[0]
+del titles[0]
+del lastn[0]
+del email[0]
+del firstn[0]
+
+degree = [d.replace('.', '') for d in degree]
+degree = [d.lstrip() for d in degree]
+
+            
+for t in range(len(titles)):
+    tt = titles[t]
+    titles[t] = tt[:-17] 
+    
+lastn = [l.split()[-1] for l in lastn]    
+lastn=tuple(lastn)  
+
+firstn = [f.split()[0] for f in firstn]
+firstn = [f.lstrip() for f in firstn]
+firstn = tuple(firstn)      
+
+val=zip(firstn,lastn,degree,titles,email)
+
+faculty_dict = dict()
+
+for line in val:
+    if line[0:2] in faculty_dict:
+        faculty_dict[line[0:2]].append(line[2:])
+    else:
+        faculty_dict[line[0:2]] = [line[2:]]
+
+x = itertools.islice(faculty_dict.items(), 0, 3)
+
+for key, value in x:
+    print key, value
+    
