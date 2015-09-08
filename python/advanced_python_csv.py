3fa0f1a46d8b16a21c66d163326409320ff047a8
Nilufer Polat <niluferplt@gmail.com>
2015-09-07 22:13:57 -0400

    prework - 6/8 - Part II

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,15 @@
-PLACE YOUR CODE HERE
+import csv
+
+faculty = csv.reader(open("faculty.csv", "rb"))
+
+email=[]
+
+for data in faculty:
+    email.append(data[3])
+    
+del email[0]
+
+c = csv.writer(open("emailcsv.csv", "wb"))
+
+for e in email:
+    c.writerow([e])
